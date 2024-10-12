c75d0fb40082ca05074682aa72369f8bfb277fe3
ciguliaz <ckazzgd@gmail.com>
2024-10-12 15:43:37 +0700

    create initial server

---
+++ whiteboard-server/server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "http://localhost:3000",
+    methods: ["GET", "POST"]
+  }
+});
+
+io.on('connection', (socket) => {
+  console.log('New client connected');
+  
+  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+  
+  socket.on('disconnect', () => console.log('Client disconnected'));
+});
+
+const PORT = 4000;
+server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
