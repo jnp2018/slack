5d3235c9973047680268ebe8f7fafe492204a3a1
ciguliaz <ckazzgd@gmail.com>
2024-10-12 15:45:01 +0700

    create initial client

---
+++ collab-whiteboard/src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Whiteboard from './widgets/Whiteboard';
+
+function App() {
+  return (
+    <div className='App'>
+      <Whiteboard />
+    </div>
+  );
+}
+
+export default App;
