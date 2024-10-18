ae78a5bf9d3106861fc95635406bfb563bcb11f7
ciguliaz <ckazzgd@gmail.com>
2024-10-18 22:42:41 +0700

    merge Duong's files

--- whiteboard-server/server.js
+++ whiteboard-server/server.js
@@ -6,7 +6,7 @@ const app = express();
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "*",
+    origin: "*",  // Allow requests from any origin
     methods: ["GET", "POST"]
   }
 });
