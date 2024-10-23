89d8fde24d6b768c8bfa050752b6573f6406a1da
NguyenThaiDuong2308 <03thaiduong@gmail.com>
2024-10-24 02:08:30 +0700

    Generate code fuction

--- collab-whiteboard/src/App.js
+++ collab-whiteboard/src/App.js
@@ -3,6 +3,14 @@ import Whiteboard from './widgets/Whiteboard';
 import {Routes, Route } from 'react-router-dom';
 import Form from "./widgets/Form";
 function App() {
+  const uuid=()=>{
+    let S4 = () =>{
+      return(((1+Math.random()) * 0x10000) | 0).toString(16).substring(1);
+    };
+  return(
+    S4()+S4()+"-"+S4()+"-"+S4()+"-"+S4()+"-"+S4()+S4()+S4()
+  );
+  };
   return (
     <div className='container'>
        <Form></Form>
