2ff744f205c5e84682e9b81a68cea7d1908c45e7
NguyenThaiDuong2308 <03thaiduong@gmail.com>
2024-10-24 02:06:09 +0700

    Frontend for JoinForm

--- collab-whiteboard/src/widgets/JoinForm.jsx
+++ collab-whiteboard/src/widgets/JoinForm.jsx
@@ -1,4 +1,29 @@
-const JoinRoomForm =()=>{
-    return <h1></h1>
-};
-export default JoinRoomForm;
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+const JoinRoomForm=()=>{
+    
+    return (
+      <form className="form col-md-12 mt-5">
+       <div className="form-group">
+          <input type="text" 
+          className="form-control my-2" 
+          placeholder="Enter you name"
+        
+          />
+       </div>
+       <div className="form-group">
+             <input type="text"
+             className="form-control my-2 "
+             placeholder="Enter room code"
+            
+             /> 
+       </div>
+      <button type="submit" 
+      className="mt-4 btn btn-primary btn-block form-control"
+     >
+          Join Room
+      </button>
+      </form>
+    );
+ };
+ export default JoinRoomForm;
