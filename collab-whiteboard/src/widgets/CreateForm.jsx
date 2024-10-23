7ed7d1118e985bee81fa6c57bd95fe31b97ecc5c
NguyenThaiDuong2308 <03thaiduong@gmail.com>
2024-10-24 02:33:50 +0700

    set attribute for Generate Room

--- collab-whiteboard/src/widgets/CreateForm.jsx
+++ collab-whiteboard/src/widgets/CreateForm.jsx
@@ -2,12 +2,26 @@ import React, { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';  
 const CreateRoomForm=({uuid})=>{
     const [roomId, setRoomId]=useState(uuid());
+    const[name,setName]=useState("");
+    const handleCreateRoom = (e)=>{
+        e.preventDefault();
+        const roomData = {
+           name,
+           roomId,
+           userId: uuid(),
+           host: true,
+           presenter: true,
+        };
+        console.log(roomData);
+    }
    return (
      <form className="form col-md-12 mt-5">
       <div className="form-group">
          <input type="text" 
          className="form-control my-2" 
          placeholder="Enter you name"
+         value={name}
+         onChange={(e)=>setName(e.target.value)}
          />
       </div>
       <div className="form-group border">
@@ -35,7 +49,7 @@ const CreateRoomForm=({uuid})=>{
       </div>
      <button type="submit" 
      className="mt-4 btn btn-primary btn-block form-control"
-    >
+     onClick={handleCreateRoom}>
          Generate Room
      </button>
      </form>
