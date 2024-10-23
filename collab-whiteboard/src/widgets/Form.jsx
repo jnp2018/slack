13859fac82bdd77d28b107f43dc78e96e0954208
NguyenThaiDuong2308 <03thaiduong@gmail.com>
2024-10-24 02:48:03 +0700

    similar with joinForm

--- collab-whiteboard/src/widgets/Form.jsx
+++ collab-whiteboard/src/widgets/Form.jsx
@@ -10,7 +10,7 @@ const Form=({uuid, socket, setUser})=>{
         </div>
         <div className="col-md-4 mt-5 form-box p-5 border border-primary rounded-2 mx-auto d-flex flex-column align-items-center">
             <h1 className="text-primary fw-bold">Join Room</h1>
-            <JoinRoomForm ></JoinRoomForm>
+            <JoinRoomForm uuid={uuid} socket={socket} setUser={setUser} ></JoinRoomForm>
         </div>
      </div>
   );
