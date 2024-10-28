214a400466e24b7a45b2d1eecb6ce67054d6febe
ciguliaz <ckazzgd@gmail.com>
2024-10-28 19:02:38 +0700

    convert to websocket + handle room navigate

--- collab-whiteboard/src/widgets/JoinForm.jsx
+++ collab-whiteboard/src/widgets/JoinForm.jsx
@@ -1,46 +1,65 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-const JoinRoomForm=({uuid, socket, setUser})=>{
-    const[roomId, setRoomId]=useState("");
-    const[name, setName]=useState("");
-    const navigate=useNavigate();
-   const handleRoomJoin =(e)=>{
+const JoinRoomForm = ({ uuid, socket, setUser }) => {
+  const [roomId, setRoomId] = useState("");
+  const [userName, setUserName] = useState("");
+  const navigate = useNavigate();
+  const handleRoomJoin = (e) => {
     e.preventDefault();
-   const roomData={
-    name,
-    roomId,
-    userId: uuid(),
-    host: false,
-    presenter: false,
-   };
-   setUser(roomData);
-   navigate(`/${roomId}`);
-   socket.emit("userJoined",roomData);
-   }
-    return (
-      <form className="form col-md-12 mt-5">
-       <div className="form-group">
-          <input type="text" 
-          className="form-control my-2" 
+    const requestData = {
+      userName,
+      roomId
+    };
+    setUser(requestData);
+
+    let roomJoinAcceptance = 'pending';
+    let roomFound = 'pending';
+    // socket.send("userJoinRoomRequest", requestData);
+    socket.send(JSON.stringify({ tag: 'userJoinRoomRequest', data: requestData }))
+    socket.onmessage('message', (message) => {
+
+      if (message.tag == 'roomFound') roomFound = 'found';
+      if (message.tag == 'roomNotFound') roomFound = 'notFound';
+
+      if (message.tag == 'userJoinRoomRequestAccepted') roomJoinAcceptance = 'accepted';
+      if (message.tag == 'userJoinRoomRequestRejected') roomJoinAcceptance = 'rejected';
+
+
+    })
+    if (roomFound === 'notFound') {
+      alert(`Room ${roomId} not found`)
+    } else if (roomJoinAcceptance === 'rejected') {
+      alert(`Join Request Not Accepted`)
+    } else if (roomFound === 'found' && roomJoinAcceptance === 'accepted') {
+      navigate(`/${roomId}`);
+    }
+
+  }
+
+  return (
+    <form className="form col-md-12 mt-5">
+      <div className="form-group">
+        <input type="text"
+          className="form-control my-2"
           placeholder="Enter you name"
-          value={name}
-          onChange={(e)=> setName(e.target.value)}
-          />
-       </div>
-       <div className="form-group">
-             <input type="text"
-             className="form-control my-2 "
-             placeholder="Enter room code"
-             value={roomId}
-             onChange={(e)=>setRoomId(e.target.value)}
-             /> 
-       </div>
-      <button type="submit" 
-      className="mt-4 btn btn-primary btn-block form-control"
-      onClick={handleRoomJoin}>
-          Join Room
+          value={userName}
+          onChange={(e) => setUserName(e.target.value)}
+        />
+      </div>
+      <div className="form-group">
+        <input type="text"
+          className="form-control my-2 "
+          placeholder="Enter room code"
+          value={roomId}
+          onChange={(e) => setRoomId(e.target.value)}
+        />
+      </div>
+      <button type="submit"
+        className="mt-4 btn btn-primary btn-block form-control"
+        onClick={handleRoomJoin}>
+        Join Room
       </button>
-      </form>
-    );
- };
- export default JoinRoomForm;
+    </form>
+  );
+};
+export default JoinRoomForm;
