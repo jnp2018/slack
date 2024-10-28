b73df487c4215a27bbaf7e677f2bbc4c162ab199
ciguliaz <ckazzgd@gmail.com>
2024-10-28 19:05:04 +0700

    remove uuid func + adjust user effect

--- collab-whiteboard/src/App.js
+++ collab-whiteboard/src/App.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from 'react';
 import Whiteboard from './widgets/Whiteboard';
 import { Routes, Route } from 'react-router-dom';
 import Form from "./widgets/Form";
-import io from 'socket.io-client';
 
 // const socket =
 //   io('https://whiteboard-server.up.railway.app/');
@@ -13,28 +12,28 @@ function App() {
   const [user, setUser] = useState(null);
   useEffect(() => {
     socket.onmessage = (msg) => {
-      if (msg.tag == 'userJoinRoomRequestAccepted') {
-        console.log('userJoinRoomRequestAccepted')
-        //TODO: handle accepted logic
-      } else {
-        console.log('userJoinRoomRequestRejected')
-        //TODO: handle rejected logic
-      }
+      // if (msg.tag == 'userJoinRoomRequestAccepted') {
+      //   console.log('userJoinRoomRequestAccepted')
+      //   //TODO: handle accepted logic
+      // } else {
+      //   console.log('userJoinRoomRequestRejected')
+      //   //TODO: handle rejected logic
+      // }
     }
   }, []);
-  const uuid = () => {
-    let S4 = () => {
-      return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
-    };
-    return (
-      S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4()
-    );
-  };
+  // const uuid = () => {
+  //   let S4 = () => {
+  //     return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
+  //   };
+  //   return (
+  //     S4() + S4() + "-" + S4() + "-" + S4() + "-" + S4() + "-" + S4() + S4() + S4()
+  //   );
+  // };
   return (
     <div className='container'>
       <div className="container">
         <Routes>
-          <Route path="/" element={<Form uuid={uuid} socket={socket} setUser={setUser} ></Form>}></Route>
+          <Route path="/" element={<Form socket={socket} setUser={setUser} ></Form>}></Route>
           <Route path="/:roomId" element={<Whiteboard socket={socket}></Whiteboard>}></Route>
         </Routes>
       </div>
