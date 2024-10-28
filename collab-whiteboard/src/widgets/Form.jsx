fd95f551dc5151eed5d8f8d62f2d7b561085116c
ciguliaz <ckazzgd@gmail.com>
2024-10-28 21:59:30 +0700

    update Form.jsx to use WSContext

--- collab-whiteboard/src/widgets/Form.jsx
+++ collab-whiteboard/src/widgets/Form.jsx
@@ -1,16 +1,20 @@
+import React, { useContext } from 'react';
+import { WebSocketContext } from '../WebSocketContext'; // Import the WebSocket context
 import CreateRoomForm from "./CreateForm";
-import JoinRoomForm from "./JoinForm";
 import './Form.css';
-const Form = ({ socket, setUser }) => {
+import JoinRoomForm from "./JoinForm";
+
+const Form = ({ setUser }) => {
+  const socket = useContext(WebSocketContext); // Access the WebSocket instance from context
   return (
     <div className="row h-100 pt-5">
       <div className="col-md-4 mt-5 form-box p-5 border border-primary rounded-2 mx-auto d-flex flex-column align-items-center">
         <h1 className="text-primary fw-bold">Create Room</h1>
-        <CreateRoomForm socket={socket} setUser={setUser}></CreateRoomForm>
+        <CreateRoomForm setUser={setUser}></CreateRoomForm> {/* remove socket={socket} */}
       </div>
       <div className="col-md-4 mt-5 form-box p-5 border border-primary rounded-2 mx-auto d-flex flex-column align-items-center">
         <h1 className="text-primary fw-bold">Join Room</h1>
-        <JoinRoomForm socket={socket} setUser={setUser} ></JoinRoomForm>
+        <JoinRoomForm setUser={setUser} ></JoinRoomForm> {/* remove socket={socket} */}
       </div>
     </div>
   );
