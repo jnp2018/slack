3d7533ea7c21bb629dfba285f0a8d33f5e9e8e41
ciguliaz <ckazzgd@gmail.com>
2024-10-29 11:15:46 +0700

    update JoinForm to use WSContext
    - able to receive message

--- collab-whiteboard/src/widgets/JoinForm.jsx
+++ collab-whiteboard/src/widgets/JoinForm.jsx
@@ -1,9 +1,16 @@
-import { useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-const JoinRoomForm = ({ uuid, socket, setUser }) => {
+import { WebSocketContext } from '../WebSocketContext'; // Import the WebSocket context
+const JoinRoomForm = ({ setUser }) => {
   const [roomId, setRoomId] = useState("");
   const [userName, setUserName] = useState("");
   const navigate = useNavigate();
+  // const socket = useContext(WebSocketContext); // Access the WebSocket instance from context
+  const { message, sendMessage } = useContext(WebSocketContext)
+
+  let roomJoinAcceptance = 'pending';
+  let roomFound = 'pending';
+
   const handleRoomJoin = (e) => {
     e.preventDefault();
     const requestData = {
@@ -11,21 +18,27 @@ const JoinRoomForm = ({ uuid, socket, setUser }) => {
       roomId
     };
     setUser(requestData);
-
-    let roomJoinAcceptance = 'pending';
-    let roomFound = 'pending';
+    sendMessage('userJoinRoomRequest', requestData)
     // socket.send("userJoinRoomRequest", requestData);
-    socket.send(JSON.stringify({ tag: 'userJoinRoomRequest', data: requestData }))
-    socket.onmessage('message', (message) => {
+    // socket.send(JSON.stringify({ tag: 'userJoinRoomRequest', data: requestData }))
+  }
+  useEffect(() => {
+    if (!message) {
+      console.log('no message')
+      return
+    }
+    console.log(`Received message =>_${message}_<=`);
+    if (message.tag == 'roomFound') roomFound = 'found';
+    if (message.tag == 'roomNotFound') roomFound = 'notFound';
 
-      if (message.tag == 'roomFound') roomFound = 'found';
-      if (message.tag == 'roomNotFound') roomFound = 'notFound';
+    if (message.tag == 'userJoinRoomRequestAccepted') roomJoinAcceptance = 'accepted';
+    if (message.tag == 'userJoinRoomRequestRejected') roomJoinAcceptance = 'rejected';
 
-      if (message.tag == 'userJoinRoomRequestAccepted') roomJoinAcceptance = 'accepted';
-      if (message.tag == 'userJoinRoomRequestRejected') roomJoinAcceptance = 'rejected';
-
-
+    console.log({
+      roomJoinAcceptance: roomJoinAcceptance,
+      roomFound: roomFound
     })
+
     if (roomFound === 'notFound') {
       alert(`Room ${roomId} not found`)
     } else if (roomJoinAcceptance === 'rejected') {
@@ -33,8 +46,7 @@ const JoinRoomForm = ({ uuid, socket, setUser }) => {
     } else if (roomFound === 'found' && roomJoinAcceptance === 'accepted') {
       navigate(`/${roomId}`);
     }
-
-  }
+  }, [message]) // Effect update on message change
 
   return (
     <form className="form col-md-12 mt-5">
